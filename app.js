fef42994a26cb972279c65ef764c20f09903602e
zaindhedhi <zaindhedhi@oulook.com>
2024-07-07 13:22:29 +0500

    q.no3-done

---
+++ app.js
@@ -0,0 +1,28 @@
+// var num1 = 6
+
+// if(num1 % 3 === 0){
+//     console.log('The Number is divisible by 3')
+// }
+// else if(num1 % 3 !== 0){
+//     console.log('The Number is Not divisible by 3')
+// }
+
+
+// var age = prompt('Enter your Age :')
+
+// if(age % 2 == 0){
+//     console.log('Even Number')
+// }else{
+//     console.log('Odd Number')
+// }
+
+var age = prompt('Enter Your Age :')
+
+if(age > 50){
+    console.log('Senior Citizen')
+} else if(age > 18){
+    console.log('Old Enough')
+}
+else{
+    console.log('Too Young')
+}
